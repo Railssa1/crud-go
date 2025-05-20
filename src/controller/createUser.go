9d0da604170991684b08293799641f5c293ebc51
Raissa <raissamalves@gmail.com>
2025-05-20 16:17:07 -0300

    Adicionando log na aplicação

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,21 +1,33 @@
 package controller
 
 import (
-	"fmt"
+	"net/http"
 
+	"github.com/Railssa1/crud-go/src/config/logger"
 	"github.com/Railssa1/crud-go/src/config/validation"
 	"github.com/Railssa1/crud-go/src/models"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
+	logger.Info("Init CreateUser controller")
 	var userRequest models.UserRequest
 
 	if err := c.BindJSON(&userRequest); err != nil {
+		logger.Error("Error trying to validate user info", err)
 		restErr := validation.ValidateUserError(err)
 		c.JSON(restErr.Code, restErr)
 		return
 	}
 
-	fmt.Println("Usuário criado", userRequest)
+	userCreate := models.UserResponse{
+		Id:    1,
+		Email: userRequest.Email,
+		Name:  userRequest.Name,
+		Age:   userRequest.Age,
+	}
+
+	logger.Info("User created successfully")
+
+	c.JSON(http.StatusOK, userCreate)
 }
